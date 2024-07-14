17dd40abe79e1d50274c401ecf52321754752443
Deeptesh Biswas <67312636+pogo-cloud@users.noreply.github.com>
2024-07-14 21:43:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-function getFormvalue() {
-    //Write your code here
+
+function getFormValue(event) {
+	let form=document.querySelector("#form1");
+	// form.addEventListener("onsubmit",abc);
+	
+	event.preventDefault();
+    let firstname=document.querySelector("#f-name").value;
+	let lastname=document.querySelector("#l-name").value;
+	console.log(firstname+" "+lastname);
+	debugger;
+	if(firstname!=""&&lastname!=""){
+		alert(firstname+" "+lastname);
+		return;
+	}
+	function abc(event){
+		event.preventdefault();
+		console.log("HI");
+		
+	}
+	
 
 }
